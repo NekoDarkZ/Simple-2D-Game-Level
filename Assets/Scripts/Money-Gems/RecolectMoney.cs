517d6d8cd167ba3cea848a3855c1183909cd8059
Cloudxz1 <vanesa.briones@alumnos.ucn.cl>
2021-09-19 13:33:48 -0300

    a
    a

--- Assets/Scripts/Money-Gems/RecolectMoney.cs
+++ Assets/Scripts/Money-Gems/RecolectMoney.cs
@@ -8,14 +8,14 @@ public class RecolectMoney : MonoBehaviour
     private float coins = 0;
     public TextMeshProUGUI textCoins;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
-
         if (other.transform.tag == "Coins")
         {
-            coins++;
             textCoins.text = coins.ToString();
+            coins++;
             Destroy(other.gameObject);
         }
     }
+
 }
